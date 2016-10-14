a91b16af50cd7d4195e7487b7943130ae3ace361
nick.zhuyj <nick.zhuyj@cn.ibm.com>
2016-10-14 14:29:56 +0800

    jokes from qiubai

--- pages/joke/joke.js
+++ pages/joke/joke.js
@@ -1,3 +1,21 @@
 Page({
-    
+    data: {
+        jokes: []
+    },
+
+    onLoad: function () {
+    console.log('joke onLoad')
+    var that = this
+    wx.request({
+      url: 'http://192.168.0.105:8080/api/humor.json',
+      success: function(res) {
+        that.setData({jokes: res.data.data})
+      }
+    })
+    },
+
+    onPullDownRefresh: function() {
+    // Do something when pull down
+    console.log("pulldown")
+    }    
 })
